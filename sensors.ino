18a5f820fb410046522a9692919de8736b4deffe
Evan <evanmwaite@gmail.com>
2023-05-22 20:19:08 -0500

    Combined sensor code

---
+++ sensors.ino
@@ -0,0 +1,57 @@
+#include "DHT.h"
+
+#define DHTPIN 2  
+#define UVPIN 36
+#define DHTTYPE DHT22  
+
+uint32_t sensorMilliVolt;
+
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("DHTxx test!"));
+
+  dht.begin();
+}
+
+void loop() {
+  // sensor is slow to update readings
+  delay(2000);
+
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  sensorMilliVolt = analogReadMilliVolts(UVPIN);
+  // Serial.printf("Sensor voltage: %d mV\n", sensorMilliVolt);
+  Serial.printf("UV index: %d\n", sensorMilliVolt * 10 / 1000);
+  // Serial.printf("UV diode current: %f μA\n\n", (double) sensorMilliVolt / 1000 / 4.3);
+  // delay(2000);
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(f);
+  Serial.print(F("°F  Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C "));
+  Serial.print(hif);
+  Serial.println(F("°F"));
+}
